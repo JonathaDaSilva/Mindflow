a1153aca3c5808a42accad4834420f534adb0b80
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2026-05-09 14:52:11 -0300

    Refac: Backend em springboot

---
+++ backend/mindflow-api/src/test/java/br/com/mindflow/MindflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mindflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const AuthController = require('../controllers/auth.controller');
-
-const routes = Router();
-
-routes.post('/register', AuthController.register);
-routes.post('/login', AuthController.login);
-
-module.exports = routes;
